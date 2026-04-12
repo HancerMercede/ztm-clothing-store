fa4a569970710fb184e9dd7dbac1fdb018b86914
Hancer <hancermercedes@gmail.com>
2026-04-11 22:40:14 -0400

    adding new media queries

--- src/components/categories/CategoriesList.tsx
+++ src/components/categories/CategoriesList.tsx
@@ -11,7 +11,3 @@ export const CategoriesList = ({ categories }: CategoriesListProps) => {
     </div>
   );
 };
-
-const fetchFn = async () => {
-  return await fetch("https://jsonplaceholder.typicode.com/users");
-};
